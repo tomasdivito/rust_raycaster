f910a80cb8903818422ec8a3628c74242cb75b2e
Tomas Di Vito <tomasdv2@gmail.com>
2018-08-18 13:51:29 -0300

    removed String::from from the string declarations for the header and the newline

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
 mod vecmath;
-mod raymath;
 
 use std::fs::File;
 use std::io::prelude::*;
 use vecmath::Vec3;
-use raymath::Ray;
 
 fn main() {
     let mut ray_file = File::create("raycast.ppm").unwrap();
     let width = 200;
     let height = 100;
-    let header = String::from(format!("P3\n{} {}\n255\n", width, height));
+    let header = format!("P3\n{} {}\n255\n", width, height);
     ray_file.write_all(header.as_bytes()).unwrap();
     for x in 0..height {
         for y in 0..width {
@@ -19,7 +17,7 @@ fn main() {
                 (x as f32 / height as f32) * 255.9,
                 0.2 * 255.9
             );
-            ray_file.write_all(String::from(format!("{}\n", color)).as_bytes()).unwrap();
+            ray_file.write_all(format!("{}\n", color).as_bytes()).unwrap();
         }
     }
 }
